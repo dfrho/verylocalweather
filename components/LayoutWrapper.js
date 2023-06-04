c5c695b6466ed993137090eb8b5cfc33fe1b3159
David Rhodes <david@espressocode.tech>
2023-06-04 00:33:17 -0500

    header margin adjusted

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -14,7 +14,7 @@ const LayoutWrapper = ({ children }) => {
   return (
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-10">
+        <header className="flex items-center justify-between pt-10">
           <div>
             <Link href="/" aria-label={siteMetadata.title}>
               <div className="flex items-center">
