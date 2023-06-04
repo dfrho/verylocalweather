dc8e31c88d5e4c0b40170428e8892d9d8752138e
David Rhodes <david@espressocode.tech>
2023-06-04 09:22:18 -0500

    comments out headers

--- next.config.js
+++ next.config.js
@@ -6,41 +6,41 @@ const ContentSecurityPolicy =
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src * blob: data:; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; media-src 'none'; connect-src *; frame-src https://www.youtube.com https://vercel.live;"
 
 const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy,
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
+  // // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  // {
+  //   key: 'Content-Security-Policy',
+  //   value: ContentSecurityPolicy,
+  // },
+  // // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  // {
+  //   key: 'Referrer-Policy',
+  //   value: 'strict-origin-when-cross-origin',
+  // },
+  // // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  // {
+  //   key: 'X-Frame-Options',
+  //   value: 'DENY',
+  // },
+  // // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  // {
+  //   key: 'X-Content-Type-Options',
+  //   value: 'nosniff',
+  // },
+  // // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  // {
+  //   key: 'X-DNS-Prefetch-Control',
+  //   value: 'on',
+  // },
+  // // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  // {
+  //   key: 'Strict-Transport-Security',
+  //   value: 'max-age=31536000; includeSubDomains',
+  // },
+  // // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  // {
+  //   key: 'Permissions-Policy',
+  //   value: 'camera=(), microphone=(), geolocation=()',
+  // },
 ]
 
 module.exports = withBundleAnalyzer({
@@ -56,7 +56,7 @@ module.exports = withBundleAnalyzer({
     return [
       {
         source: '/(.*)',
-        headers: '',
+        headers: securityHeaders,
       },
     ]
   },
