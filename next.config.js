210ad05afc7338e29e93d158f05dc1465c029f06
David Rhodes <david@espressocode.tech>
2023-06-04 00:37:54 -0500

    removes unsafe inline

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval';
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   img-src * blob: data:;
   font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
