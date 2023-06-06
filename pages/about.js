290a3d50fe6ac9c8a3578c5f5c34565def4ca706
David Rhodes <david@espressocode.tech>
2023-06-05 23:50:28 -0500

    cleans out unused sitemetadata

--- pages/about.js
+++ pages/about.js
@@ -59,6 +59,7 @@ export default function About({ authorDetails }) {
           <div className="flex w-full flex-col md:w-2/3">
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{title}</div>
+            <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
